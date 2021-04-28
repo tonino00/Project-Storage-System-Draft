732fa976e623c99147a4f7cc4d5f1bddd9c312c4
tonino00 <antoninocgama@hotmail.com>
2021-04-28 16:34:33 -0300

    ajuste nas datas

--- models/article.js
+++ models/article.js
@@ -1,55 +1,57 @@
-const mongoose = require('mongoose')
-const marked = require('marked')
-const slugify = require('slugify')
-const moment = require('moment')
+const mongoose = require('mongoose');
+const marked = require('marked');
+const slugify = require('slugify');
+const moment = require('moment');
 
 const articleSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    fullname: {
-        type: String,
-        required: true
-    },
-    viabilidade: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    startdate: {
-        type: String
-    },
-    enddate: {
-        type: String
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    situation: {
-        type: String,
-        required: true
-    },
-    slug: {
-        type: String,
-        required: true,
-        unique: true
-    }
-})
+	title: {
+		type: String,
+		required: true,
+	},
+	fullname: {
+		type: String,
+		required: true,
+	},
+	viabilidade: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+	},
+	startdate: {
+		type: String,
+		default: moment(new Date()).locale('pt-br').format('DD/MM/YYYY'),
+	},
+	enddate: {
+		type: String,
+		default: moment(new Date()).locale('pt-br').format('DD/MM/YYYY'),
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	createdAt: {
+		type: String,
+		default: moment(new Date()).locale('pt-br').format('DD/MM/YYYY'),
+	},
+	situation: {
+		type: String,
+		required: true,
+	},
+	slug: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+});
 
-articleSchema.pre('validate', function(next) {
-    if (this.title) {
-        this.slug = slugify(this.title, { lower: true, strict: true })
-    }
+articleSchema.pre('validate', function (next) {
+	if (this.title) {
+		this.slug = slugify(this.title, { lower: true, strict: true });
+	}
 
-    next()
-})
+	next();
+});
 
-module.exports = mongoose.model('Article', articleSchema)
+module.exports = mongoose.model('Article', articleSchema);
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
+const moment = require('moment');
 
 mongoose.connect('mongodb://localhost/projects', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+app.use((req, res, next) => {
+	res.locals.moment = moment;
+	next();
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({ viabilidade: 'desc' })
-    res.render('articles/index', { articles: articles })
-})
+	const articles = await Article.find().sort({ viabilidade: 'desc' });
+	res.render('articles/index', { articles: articles });
+});
 
-app.use('/articles', articleRouter)
+app.use('/articles', articleRouter);
 
-app.listen(5000)
+app.listen(5000);
